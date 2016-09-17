bb9a3c3569a590afbfe5a552e80d5d1361e34bd7
kisenka <qtuzov@gmail.com>
2016-09-17 22:21:49 +0300

    Export all stuff in package entry point

---
+++ index.js
@@ -0,0 +1,15 @@
+exports.addEntry = require('./lib/addEntry');
+exports.ChildCompiler = require('./lib/ChildCompiler');
+exports.compileVMScript = require('./lib/compileVMScript');
+exports.emitAsset = require('./lib/emitAsset');
+exports.getAffectedFiles = require('./lib/getAffectedFiles');
+exports.getAssetsByChunkName = require('./lib/getAssetsByChunkName');
+exports.InMemoryCompiler = require('./lib/InMemoryCompiler');
+exports.interpolateName = require('./lib/interpolateName');
+exports.matcher = require('./lib/matcher');
+exports.MemoryFileSystem = require('./lib/MemoryFileSystem');
+exports.MockedLoaderContext = require('./lib/MockedLoaderContext');
+exports.readFile = require('./lib/readFile');
+exports.stringifyLoaderConfig = require('./lib/stringifyLoaderConfig');
+exports.stringifyLoaderRequest = require('./lib/stringifyLoaderRequest');
+exports.TemplateCompiler = require('./lib/TemplateCompiler');
